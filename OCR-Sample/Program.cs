acae1ba9d605f84d139214128c8382b403534197
birkanogecan <birkanogecan@gmail.com>
2019-10-23 14:28:03 +0300

    Add project files.

---
+++ OCR-Sample/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using Tesseract;
+namespace OCR_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = string.Empty;
+            try
+            {
+                using (var engine = new TesseractEngine(@"C:\Users\birkan\Source\Repos\OCR-Sample\OCR-Sample\tessdata", "tur", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromFile(@"C:\Users\birkan\Source\Repos\OCR-Sample\OCR-Sample\test.png"))
+                    {
+                        using (var page = engine.Process(img))
+                        {
+
+                            using (var iter = page.GetIterator())
+                            {
+                                iter.Begin();
+
+                                do
+                                {
+                                    do
+                                    {
+                                        do
+                                        {
+                                            do
+                                            {
+
+
+                                                string buffer = iter.GetText(PageIteratorLevel.Word);
+                                                if (!(buffer == "" || buffer == null || buffer == string.Empty || buffer == " "))
+                                                {
+                                                    text += " " + iter.GetText(PageIteratorLevel.Word);
+                                                }
+
+
+                                                if (iter.IsAtFinalOf(PageIteratorLevel.TextLine, PageIteratorLevel.Word))
+                                                {
+                                                    if (!(text == " " || text == String.Empty))
+                                                    {
+                                                        text += " ";
+                                                    }
+
+                                                }
+                                            } while (iter.Next(PageIteratorLevel.TextLine, PageIteratorLevel.Word));
+
+                                            if (iter.IsAtFinalOf(PageIteratorLevel.Para, PageIteratorLevel.TextLine))
+                                            {
+                                                if (!(text == "" || text == String.Empty))
+                                                {
+                                                    text += " ";
+                                                }
+
+                                            }
+                                        } while (iter.Next(PageIteratorLevel.Para, PageIteratorLevel.TextLine));
+                                    } while (iter.Next(PageIteratorLevel.Block, PageIteratorLevel.Para));
+                                } while (iter.Next(PageIteratorLevel.Block));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                text = "";
+            }
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
